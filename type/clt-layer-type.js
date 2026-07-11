8243877bd653b9a31ed4ac5891a98229f33d15ce
DaleyDevlin <daleydevlin01@gmail.com>
2026-07-11 21:53:35 +0700

    Update clt-layer-type.js

--- type/clt-layer-type.js
+++ type/clt-layer-type.js
@@ -1,3 +1,23 @@
 class CLTLayerType {
-    
-} 
+    constructor(grade, thickness, orientation) {
+        this.grade = grade;
+        this.thickness = thickness;
+        this.orientation = orientation;
+    }
+
+    getMaterialProperties() {
+        return MaterialGrade.getGrade(this.grade);
+    }
+
+    getE() {
+        const props = this.getMaterialProperties();
+        if (!props) return 0;
+        return this.orientation === 0 ? props.E : props.E_90;
+    }
+
+    getG() {
+        const props = this.getMaterialProperties();
+        if (!props) return 0;
+        return this.orientation === 0 ? props.G : props.G_90;
+    }
+}
