4ff8a5bfe86acbf345a21b75b5cd5ba20686d5bf
DaleyDevlin <daleydevlin01@gmail.com>
2026-07-11 21:54:29 +0700

    Update panel-properties-type.js

--- type/panel-properties-type.js
+++ type/panel-properties-type.js
@@ -1,5 +1,10 @@
 class PanelPropertiesType {
     constructor() {
-        this.layers = [];
+        this.method = '';
+        this.EI_eff = 0;
+        this.layerDetails = [];
+        this.totalThickness = 0;
+        this.beff = 0;
+        this.length = 0;
     }
 }
