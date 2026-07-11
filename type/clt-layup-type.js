ac5e63901a9993cdbfb261682630569d02bd5b1d
DaleyDevlin <daleydevlin01@gmail.com>
2026-07-11 21:53:56 +0700

    Update clt-layup-type.js

--- type/clt-layup-type.js
+++ type/clt-layup-type.js
@@ -1,11 +1,25 @@
 class CLTLayupType {
-    constructor() {
-        this.name = 'CLT Layup';
-        /**
-         * @type {CLTLayerType[]}
-         */
-        this.layers = [];
+    constructor(layers, beff, length) {
+        this.layers = layers;
+        this.beff = beff;
+        this.length = length;
     }
 
-    // function
+    getLayers() {
+        return this.layers;
+    }
+
+    isSymmetric() {
+        const n = this.layers.length;
+        for (let i = 0; i < Math.floor(n / 2); i++) {
+            const kiri = this.layers[i];
+            const kanan = this.layers[n - 1 - i];
+            if (kiri.thickness !== kanan.thickness ||
+                kiri.orientation !== kanan.orientation ||
+                kiri.grade !== kanan.grade) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
