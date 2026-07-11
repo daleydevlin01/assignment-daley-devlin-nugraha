5e7542b6304592a91752cb38059016c0ca20301e
DaleyDevlin <daleydevlin01@gmail.com>
2026-07-11 21:53:20 +0700

    Update clt-layer-properties-type.js

--- type/clt-layer-properties-type.js
+++ type/clt-layer-properties-type.js
@@ -1,5 +1,16 @@
 class CLTLayerPropertiesType {
-    constructor() {
-        
+    constructor(data) {
+        this.index = data.index;
+        this.thickness = data.thickness;
+        this.orientation = data.orientation;
+        this.grade = data.grade;
+        this.E = data.E;
+        this.G = data.G;
+        this.y_ci = data.y_ci;
+        this.d = data.d;
+        this.I = data.I;
+        this.A = data.A;
+        this.contribution = data.contribution;
+        this.gamma = data.gamma || null;
     }
 }
