e33dbfe903893535c2104695d4707aba62307302
DaleyDevlin <daleydevlin01@gmail.com>
2026-07-11 21:54:12 +0700

    Update material-grade-type.js

--- type/material-grade-type.js
+++ type/material-grade-type.js
@@ -1,3 +1,10 @@
+const MATERIAL_GRADES = {
+    'MGP10': { E: 1100, E_90: 110, G: 687.5, G_90: 62.5 },
+    'MGP12': { E: 1100, E_90: 110, G: 687.5, G_90: 62.5 },
+};
+
 class MaterialGrade {
-    
+    static getGrade(gradeName) {
+        return MATERIAL_GRADES[gradeName] || null;
+    }
 }
